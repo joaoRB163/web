45f0cdc62664962c357cd33fc260e2769c530e01
JoaoRB163 <joaoabataglion087@gmail.com>
2026-05-13 20:02:04 -0300

    consulta de dados com sql via PHP

--- ppi_2/PHP/aula3/conexao.php
+++ ppi_2/PHP/aula3/conexao.php
@@ -13,7 +13,13 @@
             VALUES('$nome', '$email')";
             
     $conexao->query($sql);
+    
+    echo "Registro salvo com sucesso! <br>";
+    
+    $sql = "SELECT * FROM alunos";
+    $resultado = $conexao->query($sql);
+    while($linha = $resultado->fetch_assoc()) {
+        echo $linha['nome'] . '  |  ' . $linha['email'] . '<br>';
+    }
     $conexao->close();
-
-    echo "Registro salvo com sucesso!";
 ?>
