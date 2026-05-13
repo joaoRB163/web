0d59434a75c2ca8ca4aec6f088c246b934416c7d
JoaoRB163 <joaoabataglion087@gmail.com>
2026-05-13 19:26:03 -0300

    realizar conexão com servidor

---
+++ ppi_2/PHP/aula3/conexao.php
@@ -0,0 +1,8 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "cadastro";
+    $port = 3307;
+    $conexao = new mysqli($host, $user, $password, $database, $port);
+?>
