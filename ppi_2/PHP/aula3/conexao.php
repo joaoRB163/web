5e38afc0d4b846a7de17e221b0366826096caec7
JoaoRB163 <joaoabataglion087@gmail.com>
2026-05-13 19:33:54 -0300

    inserindo dados no BD via PHP

--- ppi_2/PHP/aula3/conexao.php
+++ ppi_2/PHP/aula3/conexao.php
@@ -5,4 +5,9 @@
     $database = "cadastro";
     $port = 3307;
     $conexao = new mysqli($host, $user, $password, $database, $port);
+
+    $sql = "INSERT INTO alunos(nome, email)
+    VALUES('teste', 'teste@gmail.com')";
+    $conexao->query($sql);
+    $conexao->close();
 ?>
